5070d6c5e33a0b44c171b31775dabb6ca47a0b51
Taylor Dronfield <tay.dronfield@gmail.com>
2021-02-03 17:48:13 -0500

    user login from pans tuts

---
+++ admin/admin_logout.php
@@ -0,0 +1,6 @@
+<?php
+require_once '../load.php';
+confirm_logged_in(); // if someone is not logged in, they cannot log out.
+
+logout();
+
--- load.php
+++ load.php
@@ -6,7 +6,11 @@ define('ADMIN_SCRIPT_PATH', ADMIN_PATH.'/scripts');
 # Display Errors, DEBUG ONLY. Turn off in production env.
     ini_set('display_errors', 1);
 
-    # Anything that is a script should go here
-    require_once ABSPATH.'/config/database.php';
-    require_once ADMIN_SCRIPT_PATH.'/read.php';
-    require_once ADMIN_SCRIPT_PATH.'/login.php';
+# Start the session 
+session_start();
+
+# Anything that is a script should go here
+require_once ABSPATH.'/config/database.php';
+require_once ADMIN_SCRIPT_PATH.'/functions.php';
+require_once ADMIN_SCRIPT_PATH.'/read.php';
+require_once ADMIN_SCRIPT_PATH.'/login.php';
